6b1865f1a3d4adf701ace4daa4aaeb1da04e820b
habibfedaee <habib.fedaee@gmail.com>
2023-05-16 22:14:37 -0700

    before fix

---
+++ api/posts/posts-routers.js
@@ -0,0 +1,85 @@
+// implement your posts router here
+const express = require("express");
+const Posts = require("./posts-model");
+
+const router = express.Router();
+
+// Posts Endpoints
+
+// get All posts: --------------------------
+router.get("/", (req, res) => {
+  Posts.find(req.query)
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "The posts information could not be retrieved",
+      });
+    });
+});
+
+// get post by id: --------------------------
+router.get("/:id", (req, res) => {
+  Posts.findById(req.params.id)
+    .then((post) => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({
+          message: "The post information could not be retrieved",
+        });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ message: "Error retrieving the post" });
+    });
+});
+
+// post insert: ------------------------------------
+router.post("/api/posts", (req, res) => {
+  const post = req.body;
+  if (!post.title || !post.contents) {
+    res.status(400).json({ message: "Please provide title and contents" });
+  } else {
+    Posts.insert(post)
+      .then((createdPost) => {
+        res.status(201).json(createdPost);
+      })
+      .catch((error) => {
+        res.status(500).json({
+          message: "There was an error while saving the post",
+          error: error.message,
+          stack: error.stack,
+        });
+      });
+  }
+});
+
+// post update by id: --------------------------------
+router.put("/:id", async (req, res) => {
+  try {
+    const possiblePost = await Posts.findById(req.params.id);
+    const post = req.body;
+    if (!possiblePost) {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist" });
+    } else if (!post.title || !post.contents) {
+      res.status(400).json({
+        message: "Please provide title and contents for the post",
+      });
+    } else {
+      const updatedPost = await Posts.update(req.params.id, post);
+      res.status(200).json(updatedPost);
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "The post information could not be modified",
+      error: error.message,
+      stack: error.stack,
+    });
+  }
+});
--- api/server.js
+++ api/server.js
@@ -1,2 +1,18 @@
 // implement your server here
 // require your posts router and connect it here
+
+const express = require("express");
+
+const postsRouter = require("./posts/posts-routers");
+
+const server = express();
+
+server.use(express.json());
+server.use("/api/posts", postsRouter);
+
+// end points
+server.get("/", (req, res) => {
+  res.send(`HOME PAGE`);
+});
+
+module.exports = server;
